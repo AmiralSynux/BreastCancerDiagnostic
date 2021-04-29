6569b16d2cce7dcb0e69082137e5a9c9ec376b50
malinacrudu <malina.crudu@yahoo.com>
2021-04-29 19:44:34 +0300

    uodates

--- analysis/dataset_analysis.py
+++ analysis/dataset_analysis.py
@@ -10,7 +10,7 @@ from input import *
 # a function that reads the ground truth for the mias db(NORM,MALIGNANT,BENIGN)
 def read_mias():
     mammograms = []
-    with open("../input/mias/groundTruth.txt", "r") as reader:
+    with open("input/mias/groundTruth.txt", "r") as reader:
         for line in reader.readlines():
             line = line.split()
             mammograms.append(line[1])
@@ -20,8 +20,8 @@ def read_mias():
 # a function that reads the ground truth for the ddsm db
 # returns the numbers of each benign, malignant mammograms
 def read_ddsm():
-    benign = len(glob('../input/ddsm/benign/**/*Mask.jpg', recursive=True))
-    malignant = len(glob('../input/ddsm/malignant/**/*Mask.jpg', recursive=True))
+    benign = len(glob('input/ddsm/benign/**/*Mask.jpg', recursive=True))
+    malignant = len(glob('input/ddsm/malignant/**/*Mask.jpg', recursive=True))
     benign = ['BENIGN'] * benign
     malignant = ['MALIGNANT'] * malignant
     return benign, malignant
@@ -39,15 +39,15 @@ def diagnosis_histogram_visualization():
 
 # displays a plot with the resolutions corresponding to mammograms
 def resolution_graph():
-    benign = glob('../input/ddsm/benign/**/*Mask.jpg', recursive=True)
-    malignant = glob('../input/ddsm/malignant/**/*Mask.jpg', recursive=True)
+    benign = glob('input/ddsm/benign/**/*Mask.jpg', recursive=True)
+    malignant = glob('input/ddsm/malignant/**/*Mask.jpg', recursive=True)
     files = benign + malignant
     xDdsm, yDdsm = [], []
     for file in files:
         matrix = array(Image.open(file))
         xDdsm.append(len(matrix))
         yDdsm.append(len(matrix[0]))
-    mias = glob('../input/mias/*.pgm', recursive=True)
+    mias = glob('input/mias/*.pgm', recursive=True)
     xMias, yMias = [], []
     for file in mias:
         matrix = array(Image.open(file))
@@ -58,10 +58,10 @@ def resolution_graph():
 
 def left_right_histogram():
     leftM, leftN, leftB, rightM, rightN, rightB = [], [], [], [], [], []
-    leftB = len(glob('../input/ddsm/benign/**/*LEFT_MLO_Mask.jpg', recursive=True))
-    rightB = len(glob('../input/ddsm/benign/**/*RIGHT_MLO_Mask.jpg', recursive=True))
-    rightM = len(glob('../input/ddsm/malignant/**/*RIGHT_MLO_Mask.jpg', recursive=True))
-    leftM = len(glob('../input/ddsm/malignant/**/*LEFT_MLO_Mask.jpg', recursive=True))
+    leftB = len(glob('input/ddsm/benign/**/*LEFT_MLO_Mask.jpg', recursive=True))
+    rightB = len(glob('input/ddsm/benign/**/*RIGHT_MLO_Mask.jpg', recursive=True))
+    rightM = len(glob('input/ddsm/malignant/**/*RIGHT_MLO_Mask.jpg', recursive=True))
+    leftM = len(glob('input/ddsm/malignant/**/*LEFT_MLO_Mask.jpg', recursive=True))
     leftB = ['LEFT_BENIGN'] * leftB
     leftM = ['LEFT_MALIGNANT'] * leftM
     rightB = ['RIGHT_BENIGN'] * rightB
@@ -85,7 +85,3 @@ def left_right_histogram():
                           colors=['yellowgreen', 'olivedrab', 'goldenrod', 'gold', 'forestgreen', 'limegreen'],
                           labels=['', 'Number of cases'], rotation=10)
 
-
-left_right_histogram()
-diagnosis_histogram_visualization()
-resolution_graph()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import numpy as np
+from analysis.dataset_analysis import *
+
 np.set_printoptions(threshold=sys.maxsize)
 
 # my_tar = tarfile.open('input/all-mias.tar.gz')
 # my_tar.extractall('./input/mias')  # specify which folder to extract to
 # my_tar.close()
+left_right_histogram()
+diagnosis_histogram_visualization()
+resolution_graph()
