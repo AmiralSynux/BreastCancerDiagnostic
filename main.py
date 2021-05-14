20a0fab4a151d6f2aa0208bce1510acd93324ca3
Alex <bossalex9@yahoo.com>
2021-05-14 17:52:19 +0300

    reader++

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
 import numpy as np
 from analysis.dataset_analysis import *
+from utils.reader import read_data_img
 
 np.set_printoptions(threshold=sys.maxsize)
 
 # my_tar = tarfile.open('input/all-mias.tar.gz')
 # my_tar.extractall('./input/mias')  # specify which folder to extract to
 # my_tar.close()
-left_right_histogram()
-diagnosis_histogram_visualization()
-resolution_graph()
+# left_right_histogram()
+# diagnosis_histogram_visualization()
+# resolution_graph()
+
+read_data_img()
