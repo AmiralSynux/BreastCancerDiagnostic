bc2e55433d56c8d9633bdf9c316ffe33f94f2f05
Alex <bossalex9@yahoo.com>
2021-05-14 15:16:06 +0300

    read ddsm

--- utils/reader.py
+++ utils/reader.py
@@ -5,10 +5,25 @@ import cv2
 from analysis.dataset_analysis import read_mias
 from preprocessing.process import remove_labels
 from domain import ImageDTO
-from utils import read_pgm, get_processed_image
+from utils import read_pgm, get_processed_image, processDDSMMammogram
 
 
-def read_data():
+def read_ddsm():
+    benign = glob('../input/ddsm/benign/**/*MLO.jpg', recursive=True)
+    malignant = glob('../input/ddsm/malignant/**/*MLO.jpg', recursive=True)
+    imgs = []
+    for path in benign:
+        imgs.append(ImageDTO(processDDSMMammogram(path),"BENING"))
+    for path in malignant:
+        imgs.append(ImageDTO(processDDSMMammogram(path),"MALIGNANT"))
+    return imgs
+
+
+def write_data(images):
+    pass
+
+
+def read_data_img():
     imageDTOs = []
     mias_gt = read_mias()
     i=0
@@ -20,8 +35,10 @@ def read_data():
         imageDTO = ImageDTO(processed_img, mias_gt[i])
         imageDTOs.append(imageDTO)
         i+=1
-        print(i)
-    print(imageDTOs[0])
+    imageDTOs += read_ddsm()
+    write_data(imageDTOs)
+    return imageDTOs
 
 
-read_data()
+def read_data():
+    pass
