4057e02a2dcfaa2d0a32a5e8f54d5938e5fe0895
Alex <bossalex9@yahoo.com>
2021-06-06 21:36:35 +0300

    clear threshold

--- detect_cancer.py
+++ detect_cancer.py
@@ -18,5 +18,42 @@ def get_image_after_segmentation(filename):
     segmented_image = segmented_image.reshape(image.shape)
 
     ret, thresh = cv2.threshold(segmented_image, 125, 255, cv2.THRESH_BINARY)
+    # clear the thresh
+    clear(thresh)
     return thresh
 
+
+# flood_fill algorithm with deep
+def flood_fill(matrix, i, j, n, m, deep):
+    queue = [(i, j)]
+    queue = set(queue)
+    while len(queue) > 0:
+        indexes = queue.pop()
+        i = indexes[0]
+        j = indexes[1]
+        if i < n and j < m and matrix[i][j][0] != 0:
+            matrix[i][j] = [0, 0, 0]
+            for k in range(1, deep + 1):
+                queue.add((i + k, j))
+                queue.add((i - k, j))
+                queue.add((i, j + k))
+                queue.add((i, j - k))
+
+
+# clears the borders of a matrix. Higher the deep, higher the "cleaning power"
+def clear(matrix, deep=3):
+    n = len(matrix)
+    m = len(matrix[0])
+    for i in range(n):
+        flood_fill(matrix, i, 0, n, m, deep)
+        flood_fill(matrix, i, m - 1, n, m, deep)
+    for j in range(m):
+        flood_fill(matrix, 0, j, n, m, deep)
+        flood_fill(matrix, n - 1, j, n, m, deep)
+
+
+# used for quick testing
+def work(thresh_filepath, save_filename, deep=3):
+    img = cv2.imread(thresh_filepath)
+    clear(img, deep)
+    cv2.imwrite(save_filename, img)
